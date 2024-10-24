3ca1c330dde5887b00845e4cd55b2a3ead538c2e
cuong.pham <cuong95nd@gmail.com>
2024-10-25 00:44:47 +0700

    fix tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { AppProps } from 'next/app';
-import '../styles/globals.css';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [supabase] = useState(() => createPagesBrowserClient());
